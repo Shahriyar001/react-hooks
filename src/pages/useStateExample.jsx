45debbfdff59010cf9dc28343576085d81eb6968
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-13 20:56:20 +0600

    added function

--- src/pages/useStateExample.jsx
+++ src/pages/useStateExample.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
 const UseStateExample = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
+  const [user, setUser] = useState({ name: "", email: "" });
+  // const [name, setName] = useState();
+  // const [email, setEmail] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
-    <form>
+    <form onClick={handleSubmit}>
       <input
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setUser({ ...user, name: e.target.value })}
         type="text"
         name="name"
         id="name"
         className="border border-2"
       />
       <input
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
         type="email"
         name="email"
         id="email"
@@ -21,8 +26,8 @@ const UseStateExample = () => {
       <button className="" type="submit">
         Submit
       </button>
-      <h1>{name}</h1>
-      <h1>{email}</h1>
+      {/* <h1>{name}</h1> */}
+      {/* <h1>{email}</h1> */}
     </form>
   );
 };
