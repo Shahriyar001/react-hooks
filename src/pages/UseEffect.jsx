aa212f2d280ee27e75778212265b6a607502fe5b
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-17 23:59:12 +0600

    update useEffect

--- src/pages/UseEffect.jsx
+++ src/pages/UseEffect.jsx
@@ -1,11 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const UseEffect = () => {
-  useEffect(() => {
-    console.log("render");
-  }, []);
+  const [hidden, setHidden] = useState(false);
+  const [count, setCount] = useState(0);
+  //   useEffect(() => {
+  //     setInterval(() => {
+  //       console.log(count);
+  //       //   setCount(count);
+  //     }, 1000);
+  //   }, [count]);
 
-  return <div></div>;
+  return (
+    <div>
+      {/* <button
+        onClick={() => setHidden((prev) => !prev)}
+        className="btn bnt-primary"
+      >
+        Click
+      </button> */}
+      <h1>{count}</h1>
+    </div>
+  );
 };
 
 export default UseEffect;
