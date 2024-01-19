da427b6c3d8b9ac435cd35f68e8708cf752a5e0c
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-19 22:04:22 +0600

    added componant

--- src/Componant/CustomInput.jsx
+++ src/Componant/CustomInput.jsx
@@ -1,9 +1,11 @@
-const CustomInput = ({ className }) => {
+import { forwardRef } from "react";
+
+const CustomInput = forwardRef<any, {}>( ({ className }) => {
   return (
     <div>
       <input type="text" className={className} name="name" id="name" />
     </div>
   );
-};
+});
 
 export default CustomInput;
